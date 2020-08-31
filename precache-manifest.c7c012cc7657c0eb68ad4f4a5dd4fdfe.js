965d36337d60649e1cff6e41df31be7a97af671e
jamesNguyen <haimenguyen@gmail.com>
2020-08-30 17:03:01 -0700

    Updates

---
+++ precache-manifest.c7c012cc7657c0eb68ad4f4a5dd4fdfe.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e0ed0e21faa571f15db36493c5362aa0",
+    "url": "/portfolioJames/index.html"
+  },
+  {
+    "revision": "5e96fe52d740cdc4532b",
+    "url": "/portfolioJames/static/css/main.33a22f99.chunk.css"
+  },
+  {
+    "revision": "35056af9c7a7f539cf64",
+    "url": "/portfolioJames/static/js/2.e2090ff3.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/portfolioJames/static/js/2.e2090ff3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5e96fe52d740cdc4532b",
+    "url": "/portfolioJames/static/js/main.7ce80a15.chunk.js"
+  },
+  {
+    "revision": "ee309b11d319f94f97de",
+    "url": "/portfolioJames/static/js/runtime-main.b81fe726.js"
+  },
+  {
+    "revision": "4e10c5a43c97720e14b391763fc34bcd",
+    "url": "/portfolioJames/static/media/LightHouse.4e10c5a4.png"
+  },
+  {
+    "revision": "6acbcec97cbd95fc6301c5423cd0a9ee",
+    "url": "/portfolioJames/static/media/bannerToContent.6acbcec9.png"
+  },
+  {
+    "revision": "d541368fe89cb00e44259362b1dca9a7",
+    "url": "/portfolioJames/static/media/boxDiscordBot.d541368f.jpg"
+  },
+  {
+    "revision": "7948343df0fe55a20fafaa5c61ddddca",
+    "url": "/portfolioJames/static/media/john_jumper.7948343d.jpg"
+  },
+  {
+    "revision": "242643c89e2c1f42567101b407044e28",
+    "url": "/portfolioJames/static/media/personalWebsiteBox.242643c8.jpg"
+  },
+  {
+    "revision": "5daf204cf8c736a0ce3400b0e385709b",
+    "url": "/portfolioJames/static/media/quizify.5daf204c.jpg"
+  },
+  {
+    "revision": "1e80e63f97e34238cdae553939ef8573",
+    "url": "/portfolioJames/static/media/websiteBanner.1e80e63f.png"
+  }
+]);
