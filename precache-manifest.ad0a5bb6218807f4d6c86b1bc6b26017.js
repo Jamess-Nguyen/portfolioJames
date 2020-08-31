3b30cee870366d9249d5eb717822f58046bfb79c
jamesNguyen <haimenguyen@gmail.com>
2020-08-31 11:22:50 -0700

    Updates

---
+++ precache-manifest.ad0a5bb6218807f4d6c86b1bc6b26017.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f8c07be17e6ea80d75812b50e21760c2",
+    "url": "/portfolioJames/index.html"
+  },
+  {
+    "revision": "9028e1e66e31329ed212",
+    "url": "/portfolioJames/static/css/main.4b3d021e.chunk.css"
+  },
+  {
+    "revision": "8808ea70332270401c45",
+    "url": "/portfolioJames/static/js/2.119d0f7e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/portfolioJames/static/js/2.119d0f7e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9028e1e66e31329ed212",
+    "url": "/portfolioJames/static/js/main.82c65984.chunk.js"
+  },
+  {
+    "revision": "ee309b11d319f94f97de",
+    "url": "/portfolioJames/static/js/runtime-main.b81fe726.js"
+  },
+  {
+    "revision": "4e10c5a43c97720e14b391763fc34bcd",
+    "url": "/portfolioJames/static/media/LightHouse.4e10c5a4.png"
+  },
+  {
+    "revision": "6acbcec97cbd95fc6301c5423cd0a9ee",
+    "url": "/portfolioJames/static/media/bannerToContent.6acbcec9.png"
+  },
+  {
+    "revision": "d541368fe89cb00e44259362b1dca9a7",
+    "url": "/portfolioJames/static/media/boxDiscordBot.d541368f.jpg"
+  },
+  {
+    "revision": "7948343df0fe55a20fafaa5c61ddddca",
+    "url": "/portfolioJames/static/media/john_jumper.7948343d.jpg"
+  },
+  {
+    "revision": "242643c89e2c1f42567101b407044e28",
+    "url": "/portfolioJames/static/media/personalWebsiteBox.242643c8.jpg"
+  },
+  {
+    "revision": "5daf204cf8c736a0ce3400b0e385709b",
+    "url": "/portfolioJames/static/media/quizify.5daf204c.jpg"
+  },
+  {
+    "revision": "1e80e63f97e34238cdae553939ef8573",
+    "url": "/portfolioJames/static/media/websiteBanner.1e80e63f.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.835fc2929bdf47dceb3618a56fb53016.js"
+  "/portfolioJames/precache-manifest.ad0a5bb6218807f4d6c86b1bc6b26017.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/portfolioJames/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
