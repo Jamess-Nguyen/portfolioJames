1a6005e11b106b84ddd8a7b19aaf276c1db8736f
jamesNguyen <haimenguyen@gmail.com>
2020-08-31 14:14:50 -0700

    image fixes

--- src/components/doodlesPage.js
+++ src/components/doodlesPage.js
@@ -1,12 +1,13 @@
 import React from 'react';
-import LightHouse from "./doodles/LightHouse.png";
 import "./doodles.css";
 import Footer from "./footer/footer";
 function Doodles () {
     return(
       <div>
         <div style={{height:"50px"}}></div>
-        <img src={LightHouse} className="x1980"></img>
+        <div  className="x1980">
+          <div id="lightHouse"></div>
+        </div>
         <div className="spacer"></div>
         <Footer/>
       </div>    
