7778da02e8331e9d20b7c46d0959ebf1275000ac
jamesNguyen <haimenguyen@gmail.com>
2020-08-30 19:54:05 -0700

    randomupdates

--- src/index.js
+++ src/index.js
@@ -3,11 +3,10 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import { HashRouter as Router} from 'react-router-dom';
 ReactDOM.render(
-  <Router>
+  <React.StrictMode>
     <App />
-  </Router>,
+  </React.StrictMode>,
   document.getElementById('root')
 );
 
