965135111d7aa262f0618af15e67e010e434c9d8
jamesNguyen <haimenguyen@gmail.com>
2020-08-31 14:33:41 -0700

    lowercaseimg

--- src/components/doodlesPage.js
+++ src/components/doodlesPage.js
@@ -1,14 +1,13 @@
 import React from 'react';
+import LightHouse from "./doodles/lightHouse.png";
 import "./doodles.css";
 import Footer from "./footer/footer";
 function Doodles () {
     return(
       <div>
         <div style={{height:"50px"}}></div>
-        <div  className="x1980">
-          <div id="lightHouse"></div>
-        </div>
-        <div  className="spacer"></div>
+        <img src={LightHouse} className="x1980"></img>
+        <div className="spacer"></div>
         <Footer/>
       </div>    
     );
